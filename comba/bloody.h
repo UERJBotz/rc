d16bb5a9b62d4cdafcbe93c56f75f058832094ec
theo-al <emailtheoprauerj@gmail.com>
2026-01-21 12:29:36 -0300

    led aceso + bloody mary

---
+++ comba/bloody.h
@@ -0,0 +1,11 @@
+#if defined(COMBA) || defined(VESPA)
+    #error "inclua somente 1 robô"
+#endif
+#define COMBA
+
+#define LED LED_BUILTIN
+
+#define motor_esq_m1 5
+#define motor_esq_m2 6
+#define motor_dir_m1 3
+#define motor_dir_m2 4
