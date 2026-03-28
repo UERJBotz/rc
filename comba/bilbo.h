72bfe3be9c729f6b526af8f5f862e6833db41833
theo-al <emailtheoprauerj@gmail.com>
2026-03-27 21:31:55 -0300

    comba: adicionado bilbo e soldado*
    *do exército de abelhas da eletrônica pros beetles

---
+++ comba/bilbo.h
@@ -0,0 +1,19 @@
+#if defined(COMBA) || defined(VESPA)
+    #error "inclua somente 1 robô"
+#endif
+#define COMBA
+
+
+#define CONTROLE controle_roxo_j_verm
+
+#define LED LED_BUILTIN
+
+#define D1 5
+#define D2 4
+#define D3 0
+#define D4 2
+
+#define motor_esq_m1 D1
+#define motor_esq_m2 D2
+#define motor_dir_m1 D4
+#define motor_dir_m2 D3
---
+++ comba/soldado.h
@@ -0,0 +1,16 @@
+#if defined(COMBA) || defined(VESPA)
+    #error "inclua somente 1 robô"
+#endif
+#define COMBA
+
+// essas plaquinhas com a ponte h vermelha e o esp866
+//! chamar de abelhas em vez de soldados (brincadeira com a vespa da robocore)
+
+#define CONTROLE controle_branco
+
+#define LED LED_BUILTIN
+
+#define motor_esq_m1 D0
+#define motor_esq_m2 D5
+#define motor_dir_m1 D6
+#define motor_dir_m2 D7
