1f0efb5755cd8cd3ab6cfa9d9bdf632c4bee131b
theo-al <emailtheoprauerj@gmail.com>
2026-03-31 19:04:38 -0300

    controle: corrigido controle dracarys

--- controle/controle_preto_j_verm.h
+++ controle/controle_preto_j_verm.h
@@ -3,11 +3,11 @@
 #endif
 #define CONTROLE "preto_1j_vermelho"
 
-#define EIXO_X 3
+#define EIXO_X 1
 #define EIXO_Y 2
-#define BOTAO  10
+#define BOTAO  9
 
 #define ARMA_DIGITAL
 #define ARMA_SEC_DIGITAL
-#define EIXO_ARMA     6
-#define EIXO_ARMA_SEC 5
+#define EIXO_ARMA     3
+#define EIXO_ARMA_SEC 4
