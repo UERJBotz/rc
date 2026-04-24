9a1242fe8973ab4eb72963b7ee7a5caa6f3637b7
theo-al <emailtheoprauerj@gmail.com>
2026-04-24 18:34:13 -0300

    comba: "killbot" adicionado
    esse na verdade é tanto a bloody quanto o nosfepato

---
+++ comba/killbot.h
@@ -0,0 +1,16 @@
+#if defined(COMBA) || defined(VESPA)
+    #error "inclua somente 1 robô"
+#endif
+#define COMBA
+
+// Nologo ESP32C3 Super Mini
+
+
+#define CONTROLE controle_branco
+
+#define LED LED_BUILTIN
+
+#define motor_esq_m1 5
+#define motor_esq_m2 6
+#define motor_dir_m1 9
+#define motor_dir_m2 10
