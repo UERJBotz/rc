40f19f15a5415a0ae22cf403c4c095a7d5325416
theo-al <emailtheoprauerj@gmail.com>
2026-07-24 13:02:47 -0300

    controle: branco com mais switches
    agora dá pra usar com arma

--- controle/controle_branco.h
+++ controle/controle_branco.h
@@ -7,7 +7,12 @@
 #define EIXO_Y 1
 #define BOTAO  9
 
-#define INTERRUPTOR_INVERTER         6
-#define INTERRUPTOR_INVERTER_ESQ_DIR 7
+#define ARMA_DIGITAL
+#define ARMA_SEC_DIGITAL
+#define EIXO_ARMA     6
+#define EIXO_ARMA_SEC 7
+
+#define INTERRUPTOR_INVERTER         9
+#define INTERRUPTOR_INVERTER_ESQ_DIR 8
 
 #define PONTO_ZERO { .x = 2815, .y = 2290 }
