fce1b9c4551a94981546ad240500d4d64518b5bb
theo-al <emailtheoprauerj@gmail.com>
2026-08-04 12:35:24 -0300

    controle: controle com gatilho analógico

---
+++ controle/controle_preto_j_preto.h
@@ -0,0 +1,13 @@
+#ifdef CONTROLE
+  #error "inclua somente um controle"
+#endif
+#define CONTROLE "preto_1j_preto"
+
+#define EIXO_X 2
+#define EIXO_Y 1
+#define BOTAO  8
+
+#define EIXO_ARMA 3
+
+#define ARMA_SEC_DIGITAL
+#define EIXO_ARMA_SEC 5
